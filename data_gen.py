f28297046618602cd02be35b3ce13f81cf935659
Yang Liu <liuyang12@focusmedia.cn>
2019-08-16 14:36:14 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -36,7 +36,7 @@ class TextMelLoader(torch.utils.data.Dataset):
     def get_mel(self, filename):
         audio, sampling_rate = load_wav_to_torch(filename)
         if sampling_rate != self.stft.sampling_rate:
-            raise ValueError("{} {} SR doesn't match target {} SR".format(
+            raise ValueError("{} SR doesn't match target {} SR".format(
                 sampling_rate, self.stft.sampling_rate))
         audio_norm = audio / self.max_wav_value
         audio_norm = audio_norm.unsqueeze(0)
